893063e2d82bf69a9dad83fd15e443d5b8f6d2dc
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-01-22 11:08:02 -0500

    Basic setup of Nav components.

---
+++ apple-nav/src/components/Navigation/NavWrapper.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from './Nav';
+import SubNav from './SubNav'
+
+const NavWrapper = () => {
+    return (
+        <div>
+            <Nav />
+            <SubNav />
+        </div>
+    );
+  }
+  
+  export default NavWrapper;
