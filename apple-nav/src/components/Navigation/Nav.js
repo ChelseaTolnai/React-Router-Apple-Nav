9d4514933bc36d15d67d3d6c0e38218cf1df3d38
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-01-22 15:57:40 -0500

    Styling of parent Nav Links and mapping over data array to get parent Nav text.

--- apple-nav/src/components/Navigation/Nav.js
+++ apple-nav/src/components/Navigation/Nav.js
@@ -1,35 +1,45 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
+import { navLinkData } from '../../data/NavData'
 
+const MainNavContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: baseline;
+    padding: 1% 8% 1.2%;
+`
+const NavIcon = styled.i`
+    font-size: 1.3em;
+`
 const StyledNavLink = styled(NavLink)`
     text-decoration: none;
-    color: #FFFFFF;
-    margin: 0 2%;
+    color: ${() => navLink.color};
 `
-// const navLink = {
-//     color: "#FFFFFF",
-// };
+const navLink = {
+    color: "#FFFFFF",
+};
 
-// const activeNavLink = {
-//     color: "#B6B6B6",
-// };
+const activeNavLink = {
+    color: "#B6B6B6",
+};
 
 const Nav = () => {
-
     return (
-        <div>
-            <StyledNavLink to=""><i className="fa fa-apple"></i></StyledNavLink>
-            <StyledNavLink to="">Mac</StyledNavLink>
-            <StyledNavLink to="">iPad</StyledNavLink>
-            <StyledNavLink to="">iPhone</StyledNavLink>
-            <StyledNavLink to="">Watch</StyledNavLink>
-            <StyledNavLink to="">TV</StyledNavLink>
-            <StyledNavLink to="">Music</StyledNavLink>
-            <StyledNavLink to="">Support</StyledNavLink>
-            <StyledNavLink to=""><i className="fa fa-search"></i></StyledNavLink>
-            <StyledNavLink to=""><i className="fa fa-shopping-bag"></i></StyledNavLink>
-        </div>
+        <MainNavContainer>
+            <StyledNavLink to=""><NavIcon className="fa fa-apple"></NavIcon></StyledNavLink>
+            {navLinkData.map( item => 
+                <StyledNavLink 
+                    to="" 
+                    key={item.mainNav} 
+                >
+                    {item.mainNav}
+                </StyledNavLink>
+            )}
+            <StyledNavLink to=""><NavIcon className="fa fa-search"></NavIcon></StyledNavLink>
+            <StyledNavLink to=""><NavIcon className="fa fa-shopping-bag"></NavIcon></StyledNavLink>
+        </MainNavContainer>
     );
   }
   
--- apple-nav/src/components/Navigation/NavWrapper.js
+++ apple-nav/src/components/Navigation/NavWrapper.js
@@ -6,13 +6,16 @@ import styled from 'styled-components';
 const NavContainer = styled.div`
 background-color: #313131;
 color: #FFFFFF;
+font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+font-size: 62.5%;
+font-size: 0.9em;
 `
 
 const NavWrapper = () => {
     return (
         <NavContainer>
             <Nav />
-            <SubNav />
+            {/* <SubNav /> */}
         </NavContainer>
     );
   }
