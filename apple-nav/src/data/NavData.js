711b3f988e330cb722c8c94f793b56a3db8b78fe
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-01-22 14:43:07 -0500

    Created NavLink Date file for all NavLinks, SubNavLinks, and images. Created image files as well.

---
+++ apple-nav/src/data/NavData.js
@@ -0,0 +1,233 @@
+export const navLinkData = [
+    {
+        mainNav: 'Mac', 
+        subNavs: [
+            {
+                img: '../images/Mac/MacBook',
+                name: 'MacBook'
+            },
+            {
+                img: '../images/Mac/MacBookAir',
+                name: 'MacBook Air',
+                isNew: true
+            },
+            {
+                img: '../images/Mac/MacBookPro',
+                name: 'MacBook Pro'
+            },
+            {
+                img: '../images/Mac/iMac',
+                name: 'iMac'
+            },
+            {
+                img: '../images/Mac/iMacPro',
+                name: 'iMac Pro'
+            },
+            {
+                img: '../images/Mac/MacPro',
+                name: 'Mac Pro'
+            },
+            {
+                img: '../images/Mac/MacMini',
+                name: 'Mac mini',
+                isNew: true
+            },
+            {
+                img: '../images/Mac/MacAccessories',
+                name: 'Accessories'
+            },
+            {
+                img: '../images/Mac/MacOS',
+                name: 'Mojave'
+            },
+            {
+                img: '../images/Mac/MacCompare',
+                name: 'Compare'
+            },
+        ]
+    }, 
+    {
+        mainNav: 'iPad', 
+        subNavs: [
+            {
+                img: '../images/iPad/iPadPro',
+                name: 'iPad Pro',
+                isNew: true
+            },
+            {
+                img: '../images/iPad/iPadPro10',
+                name: 'iPad Pro',
+                size: '10.5"'
+            },
+            {
+                img: '../images/iPad/iPad9',
+                name: 'iPad',
+                size: '9.7"'
+            },
+            {
+                img: '../images/iPad/iPadMini',
+                name: 'iPad mini 4'
+            },
+            {
+                img: '../images/iPad/iPadiOS',
+                name: 'iOS 12'
+            },
+            {
+                img: '../images/iPad/iPadPencil',
+                name: 'Apple Pencil'
+            },
+            {
+                img: '../images/iPad/iPadKeyboard',
+                name: 'Smart Keyboard'
+            },
+            {
+                img: '../images/iPad/iPadAccessories',
+                name: 'Accessories'
+            },
+            {
+                img: '../images/iPad/iPadCompare',
+                name: 'Compare'
+            },
+        ]
+    }, 
+    {
+        mainNav: 'iPhone', 
+        subNavs: [
+            {
+                img: '../images/iPhone/iPhoneXS',
+                name: 'iPhone XS',
+                isNew: true
+            },
+            {
+                img: '../images/iPhone/iPhoneXR',
+                name: 'iPhone XR',
+                isNew: true
+            },
+            {
+                img: '../images/iPhone/iPhone8',
+                name: 'iPhone 8',
+            },
+            {
+                img: '../images/iPhone/iPhone7',
+                name: 'iPhone 7'
+            },
+            {
+                img: '../images/iPhone/iPhoneiOS',
+                name: 'iOS 12'
+            },
+            {
+                img: '../images/iPhone/iPhoneAirPods',
+                name: 'AirPods'
+            },
+            {
+                img: '../images/iPhone/iPhoneAccessories',
+                name: 'Accessories'
+            },
+            {
+                img: '../images/iPhone/iPhoneCompare',
+                name: 'Compare'
+            },
+        ]
+    }, 
+    {
+        mainNav: 'Watch', 
+        subNavs: [
+            {
+                img: '../images/Watch/Watch4',
+                name: 'Apple Watch Series 4',
+                isNew: true
+            },
+            {
+                img: '../images/Watch/WatchNike',
+                name: 'Apple Watch Nike+',
+                isNew: true
+            },
+            {
+                img: '../images/Watch/WatchHermes',
+                name: 'Apple Watch Hermès',
+                isNew: true
+            },
+            {
+                img: '../images/Watch/Watch3',
+                name: 'Apple Watch Series 3'
+            },
+            {
+                img: '../images/Watch/WatchOS',
+                name: 'watchOS'
+            },
+            {
+                img: '../images/Watch/WatchBands',
+                name: 'Bands'
+            },
+            {
+                img: '../images/Watch/WatchAccessories',
+                name: 'Accessories'
+            },
+            {
+                img: '../images/Watch/WatchCompare',
+                name: 'Compare'
+            },
+        ]
+    }, 
+    {
+        mainNav: 'TV', 
+        subNavs: [
+            {
+                img: '../images/TV/TV4K',
+                name: 'Apple TV 4K',
+            },
+            {
+                img: '../images/TV/TV',
+                name: 'Apple TV',
+            },
+            {
+                img: '../images/TV/TVApp',
+                name: 'TV App',
+            },
+            {
+                img: '../images/TV/TVAccessories',
+                name: 'Accessories'
+            },
+            {
+                img: '../images/TV/TVCompare',
+                name: 'Compare'
+            },
+        ]
+    }, 
+    {
+        mainNav: 'Music', 
+        subNavs: [
+            {
+                img: '../images/Music/Music',
+                name: 'Apple Music',
+            },
+            {
+                img: '../images/Music/MusiciTunes',
+                name: 'iTunes',
+            },
+            {
+                img: '../images/Music/MusicHomePod',
+                name: 'HomePod',
+            },
+            {
+                img: '../images/Music/MusicAirBuds',
+                name: 'AirPods',
+            },
+            {
+                img: '../images/Music/MusiciPodTouch',
+                name: 'iPod touch',
+            },
+            {
+                img: '../images/Music/MusicAccessories',
+                name: 'Music Accessories'
+            },
+            {
+                img: '../images/Music/MusicCompare',
+                name: 'Gift Cards'
+            },
+        ]
+    }, 
+    {
+        mainNav: 'Support', 
+    }, 
+];
